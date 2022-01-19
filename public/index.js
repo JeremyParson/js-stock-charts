66c134198cbccfab04f39ad0cd5c36bc1281df63
Jeremy Parson <jsbparson@gmail.com>
2022-01-18 22:27:32 -0500

    first commit

--- public/index.js
+++ public/index.js
@@ -4,6 +4,45 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    // let results = await fetch(`https://api.twelvedata.com/time_series?symbol=BNTX,DIS,GME,MSFT&interval=1min&apikey=${apiKey}`)
+    // let stocks = await results.json()
+
+    let stock_data = mockData
+
+    const { BNTX, DIS, GME, MSFT } = stock_data
+
+    const stocks = [BNTX, DIS, GME, MSFT]
+
+    stocks.forEach( stock => stock.values.reverse())
+
+    // Time Chart
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.reverse().map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.reverse().map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+}
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
 }
 
 main()
